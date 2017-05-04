4c63ea5394386d3f98c8ee82d822badd30ee38a2
jachicao <jachicao@uc.cl>
2017-05-04 03:36:00 -0300

    develop-web

--- develop-deploy.sh
+++ develop-deploy.sh
@@ -10,8 +10,8 @@ docker-compose -f develop-docker-compose.yml down
 
 docker-compose -f develop-docker-compose.yml up -d
 
-docker-compose -f develop-docker-compose.yml exec -d web rake db:create
+docker-compose -f develop-docker-compose.yml exec -d develop-web rake db:create
 
-docker-compose -f develop-docker-compose.yml exec -d web rake db:migrate db:seed
+docker-compose -f develop-docker-compose.yml exec -d develop-web rake db:migrate db:seed
 
 exit 0
