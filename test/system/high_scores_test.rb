61ceca59087b944fd55ffd220bb9fbb192da23c5
jachicao <jachicao@uc.cl>
2017-05-04 20:43:12 -0300

    clean myapp

--- test/system/high_scores_test.rb
+++
@@ -1,9 +0,0 @@
-require "application_system_test_case"
-
-class HighScoresTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit high_scores_url
-  #
-  #   assert_selector "h1", text: "HighScore"
-  # end
-end
