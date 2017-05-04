51b7aea194a4a77d452ed8ffb47d7232f4c5053a
jachicao <jachicao@uc.cl>
2017-05-04 04:09:55 -0300

    port

--- develop-deploy.sh
+++ develop-deploy.sh
@@ -10,7 +10,7 @@ docker-compose -f develop-docker-compose.yml build
 
 docker-compose -f develop-docker-compose.yml down
 
-docker-compose -f develop-docker-compose.yml up -d
+docker-compose -f develop-docker-compose.yml up -d --remove-orphans
 
 docker-compose -f develop-docker-compose.yml exec -d develop-web rake db:create
 
--- master-deploy.sh
+++ master-deploy.sh
@@ -10,7 +10,7 @@ docker-compose -f master-docker-compose.yml build
 
 docker-compose -f master-docker-compose.yml down
 
-docker-compose -f master-docker-compose.yml up -d
+docker-compose -f master-docker-compose.yml up -d --remove-orphans
 
 docker-compose -f master-docker-compose.yml exec -d web rake db:create
 
