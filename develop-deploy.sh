ebd14373aaad6eaf012c27e795407e9f065c106c
jachicao <jachicao@uc.cl>
2017-05-04 02:03:45 -0300

    a

--- develop-deploy.sh
+++ develop-deploy.sh
@@ -4,14 +4,14 @@ cd "$(dirname "$0")"
 
 git pull
 
-docker-compose build
+docker-compose -f develop-docker-compose.yml build
 
-docker-compose down
+docker-compose -f develop-docker-compose.yml down
 
-docker-compose up -d
+docker-compose -f develop-docker-compose.yml up -d
 
-docker-compose exec -d web rake db:create
+docker-compose -f develop-docker-compose.yml exec -d web rake db:create
 
-docker-compose exec -d web rake db:migrate db:seed
+docker-compose -f develop-docker-compose.yml exec -d web rake db:migrate db:seed
 
 exit 0
--- master-deploy.sh
+++ master-deploy.sh
@@ -4,14 +4,14 @@ cd "$(dirname "$0")"
 
 git pull
 
-docker-compose build
+docker-compose -f master-docker-compose.yml build
 
-docker-compose down
+docker-compose -f master-docker-compose.yml down
 
-docker-compose up -d
+docker-compose -f master-docker-compose.yml up -d
 
-docker-compose exec -d web rake db:create
+docker-compose -f master-docker-compose.yml exec -d web rake db:create
 
-docker-compose exec -d web rake db:migrate db:seed
+docker-compose -f master-docker-compose.yml exec -d web rake db:migrate db:seed
 
 exit 0
