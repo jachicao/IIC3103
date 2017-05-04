380835da6c676d684b249b2c6c13958c69c482ca
jachicao <jachicao@uc.cl>
2017-05-04 01:12:14 -0300

    exec

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,14 @@ cd "$(dirname "$0")"
 
 git pull
 
-docker-compose down
-
 docker-compose build
+
+docker-compose down
 
 docker-compose up -d
 
-docker-compose run web rake db:create
+docker-compose exec web rake db:create
 
-docker-compose run web rake db:migrate db:seed
+docker-compose exec web rake db:migrate db:seed
 
 exit 0
