72e2aa9d54bb2c8c0d12a52b3f119adb8c90d4cc
jachicao <jachicao@uc.cl>
2017-05-04 23:12:42 -0300

    .env

--- master-deploy.sh
+++ master-deploy.sh
@@ -10,8 +10,6 @@ docker-compose -f master-docker-compose.yml build
 
 docker-compose -f master-docker-compose.yml down
 
-docker-compose -f master-docker-compose.yml run web /bin/bash -c "export SECRET_KEY_BASE=$(rake secret) && run_cmd"
-
 docker-compose -f master-docker-compose.yml up -d --remove-orphans
 
 docker-compose -f master-docker-compose.yml exec -d web rake db:create
