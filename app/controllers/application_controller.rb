4fd51d60b3f750466df27893f2364ce9302249f2
jachicao <jachicao@uc.cl>
2017-05-04 21:36:32 -0300

    clean

--- app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
-  #GET
-  def token
-    render json: { "token": 'abtj12312ab'}, status: 202
-  end
-end
