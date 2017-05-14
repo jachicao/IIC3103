662f9a1fb85f68f56e4b377323f2fdc357e4d6f2
jachicao <jachicao@uc.cl>
2017-05-14 01:56:36 -0400

    cd develop

--- develop-deploy.sh
+++ develop-deploy.sh
@@ -10,6 +10,8 @@ rm -rf develop
 
 git clone --branch develop https://github.com/jachicao/IIC3103.git develop
 
+cd develop
+
 docker-compose -f develop-docker-compose.yml build
 
 docker-compose -f develop-docker-compose.yml up -d --remove-orphans
