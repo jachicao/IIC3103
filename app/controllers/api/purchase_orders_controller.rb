4878a23f4885c444bf42fb4cfc377f18df8d1303
saalba <saalba@uc.cl>
2017-05-15 18:00:23 -0400

    frontend

--- app/controllers/api/purchase_orders_controller.rb
+++ app/controllers/api/purchase_orders_controller.rb
@@ -5,14 +5,20 @@ class Api::PurchaseOrdersController < ApplicationController
   # POST /purchase_orders
   # POST /purchase_orders.json
   def create
-    params[:store_reception_id] = params[:id_store_reception]
-    @purchase_order = PurchaseOrder.new({ po_id: params[:po_id], store_reception_id: params[:store_reception_id], payment_method: params[:payment_method] })
-
-    if @purchase_order.save
-      render json: @purchase_order
-    else
-      render json: @purchase_order.errors, status: :unprocessable_entity
-    end
+    req_params = { :id => params[:id] };
+    res = HTTParty.get(ENV['CENTRAL_SERVER_URL'] + '/oc/obtener', :query => req_params, :headers => { content_type: 'application/json', accept: 'application/json' } );
+    case res.code
+      when 200
+        params[:store_reception_id] = params[:id_store_reception]
+        @purchase_order = PurchaseOrder.new({ po_id: params[:id], store_reception_id: params[:store_reception_id], payment_method: params[:payment_method], status: "creada" })
+        if @purchase_order.save
+          render json: @purchase_order
+        else
+          render json: @purchase_order.errors, status: :unprocessable_entity
+        end
+      when 404
+      render json: {error: "Orden de compra inexistente"}, status: 404, content_type: "application/json"
+      end
   end
 
   # PATCH/PUT /purchase_orders/1/accepted
