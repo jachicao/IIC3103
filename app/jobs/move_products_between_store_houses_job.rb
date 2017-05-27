8e77b038a39b9d36b16b2c9c9a421dc887d801ae
jachicao <jachicao@uc.cl>
2017-05-27 04:06:38 -0400

    correct movement for products

--- app/jobs/move_products_between_store_houses_job.rb
+++ app/jobs/move_products_between_store_houses_job.rb
@@ -16,15 +16,17 @@ class MoveProductsBetweenStoreHousesJob < ApplicationJob
     $redis.del('get_almacenes')
     $redis.del('get_skus_with_stock:' + to_store_house_id)
     $redis.del('get_skus_with_stock:' + from_store_house_id)
-    limit = (quantity.to_f / 100.to_f).ceil - 1
+    limit = (quantity.to_f / 100.to_f).ceil
     total_moved = 0
-    for i in 0..limit
+    index = 0
+    while index < limit
       quantity_to_move = [quantity - total_moved, 100].min
       products = get_products(from_store_house_id, sku, quantity_to_move)
       products.each do |p|
         MoveProductInternallyJob.perform_later(p[:_id], to_store_house_id, from_store_house_id)
       end
       total_moved += quantity_to_move
+      index += 1
     end
   end
 end
