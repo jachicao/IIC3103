4930b64f70c110428d28c2cb8bc87903e5506f96
andresback <aback@uc.cl>
2017-05-30 16:03:44 -0400

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,13 @@ ActiveRecord::Schema.define(version: 20170530192867) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "factory_orders", force: :cascade do |t|
+  create_table "factory_orders", id: :bigserial, force: :cascade do |t|
     t.string   "fo_id"
     t.string   "sku"
     t.datetime "available"
     t.integer  "quantity"
     t.index ["fo_id"], name: "index_factory_orders_on_fo_id", using: :btree
   end
-
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
@@ -38,8 +37,8 @@ ActiveRecord::Schema.define(version: 20170530192867) do
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
   end
 
-  create_table "ingredients", force: :cascade do |t|
-    t.integer   "product_id"
+  create_table "ingredients", id: :bigserial, force: :cascade do |t|
+    t.bigint   "product_id"
     t.integer  "item_id"
     t.integer  "quantity"
     t.datetime "created_at", null: false
@@ -54,9 +53,7 @@ ActiveRecord::Schema.define(version: 20170530192867) do
     t.integer  "total_amount"
     t.string   "po_id"
     t.datetime "payment_date"
-    t.string   "rejected_reason"
-    t.string   "cancelled_reason"
-    t.boolean  "is_bill",          default: false
+    t.boolean  "is_bill",      default: false
   end
 
   create_table "pending_products", force: :cascade do |t|
@@ -67,27 +64,29 @@ ActiveRecord::Schema.define(version: 20170530192867) do
     t.index ["product_id"], name: "index_pending_products_on_product_id", using: :btree
   end
 
-  create_table "producers", force: :cascade do |t|
+  create_table "producers", id: :bigserial, force: :cascade do |t|
     t.string   "producer_id"
     t.integer  "group_number"
     t.string   "account"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.boolean  "me",           default: false
     t.index ["producer_id"], name: "index_producers_on_producer_id", using: :btree
   end
 
-  create_table "product_in_sales", force: :cascade do |t|
-    t.integer  "producer_id"
-    t.integer  "product_id"
+  create_table "product_in_sales", id: :bigserial, force: :cascade do |t|
+    t.bigint   "producer_id"
+    t.bigint   "product_id"
     t.integer  "price"
     t.decimal  "average_time"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.boolean  "mine"
     t.index ["producer_id"], name: "index_product_in_sales_on_producer_id", using: :btree
     t.index ["product_id"], name: "index_product_in_sales_on_product_id", using: :btree
   end
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", id: :bigserial, force: :cascade do |t|
     t.string   "sku"
     t.string   "name"
     t.string   "product_type"
@@ -98,13 +97,15 @@ ActiveRecord::Schema.define(version: 20170530192867) do
     t.datetime "updated_at",   null: false
   end
 
-  create_table "purchase_orders", force: :cascade do |t|
+  create_table "purchase_orders", id: :bigserial, force: :cascade do |t|
     t.string   "po_id"
     t.string   "payment_method"
     t.string   "store_reception_id"
     t.string   "status"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.boolean  "own"
+    t.boolean  "dispatched"
     t.index ["po_id"], name: "index_purchase_orders_on_po_id", using: :btree
   end
 
