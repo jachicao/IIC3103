74b902852154df3183f7b998e79a64bf537ba123
saalba <saalba@uc.cl>
2017-05-31 15:26:14 -0400

    Delete rejecter orders and dispatch if posible

--- app/workers/accept_purchase_orders_worker.rb
+++ app/workers/accept_purchase_orders_worker.rb
@@ -3,19 +3,21 @@ class AcceptPurchaseOrdersWorker
 
   def perform(purchase_order_id)
 
-    purchase_order = GetPurchaseOrderJob.perform_now(purchase_order_id
-    response = purchase_order[:body].first
+    server_purchase_order = GetPurchaseOrderJob.perform_now(purchase_order_id)
+    response = server_purchase_order[:body].first
     group_number = Producer.all.find_by(producer_id: response[:cliente]).group_number
     sku = response[:sku]
     cuantity = response[:cantidad]
     price = response[:precioUnitario]
     product_in_sale = ProductInSale.do_i_produce(sku)
-    
+    purchase_order = PurchaseOrder.aññ.find_by(po_id: purchase_order_id)
+
     if product_in_sale =! nil
 
       if product_in_sale.get_price(sku) < price
         RejectGroupPurchaseOrderJob.perform_now(group_number, purchase_order_id, "Precio Incorrecto")
         RejectServerPurchaseOrderJob.perform_now(purchase_order_id, "Precio Incorrecto")
+        purchase_order.destroy
       end
 
       product =  product_in_sale.product
@@ -29,9 +31,15 @@ class AcceptPurchaseOrdersWorker
       if (Time.now + time_analysis.to_f.hours).to_i * 1000 < deadline
         AcceptGroupPurchaseOrderJob.perform_now(group_number, purchase_order_id)
         AcceptServerPurchaseOrderJob.perform_now(purchase_order_id)
+
+        if time_analysis.to_f.hours == 0
+          purchase_order.dispatch_order(sku, cuantity, price)
+        end
+
       else
         RejectGroupPurchaseOrderJob.perform_now(group_number, purchase_order_id, "Tiempo insuficiente")
         RejectServerPurchaseOrderJob.perform_now(purchase_order_id, "Tiempo insuficiente")
+        purchase_order.destroy
       end
 
     end
@@ -39,6 +47,7 @@ class AcceptPurchaseOrdersWorker
   else
     RejectGroupPurchaseOrderJob.perform_now(group_number, purchase_order_id, "SKU incorrecta")
     RejectServerPurchaseOrderJob.perform_now(purchase_order_id,"SKU incorrecta")
+    purchase_order.destroy
   end
 
 end
