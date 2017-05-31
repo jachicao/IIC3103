4ca503289fea6a4704bc795c65f539ca59aa508b
jachicao <jachicao@uc.cl>
2017-05-31 14:55:18 -0400

    price instead of precio

--- app/controllers/api/api_products_controller.rb
+++ app/controllers/api/api_products_controller.rb
@@ -3,7 +3,32 @@ class Api::ApiProductsController < Api::ApiController
   # GET /products
   # GET /products.json
   def index
-    return get_stock
+    me = Producer.get_me
+    products = []
+    me.product_in_sales.each do |product_in_sale|
+      product = product_in_sale.product
+      products[product.sku.to_i] = { sku: product.sku, name: product.name, price: product_in_sale.price, stock: 0 }
+    end
+    all_stock = StoreHouse.all_stock
+    if all_stock != nil
+      all_stock.each do |store_house|
+        store_house[:inventario].each do |p|
+          product = products[p[:sku].to_i]
+          if product != nil
+            product[:stock] += p[:total]
+          end
+        end
+      end
+    end
+
+    result = []
+    products.each do |product|
+      if product != nil
+        result.push(product)
+      end
+    end
+
+    return render json: result
   end
 
 
