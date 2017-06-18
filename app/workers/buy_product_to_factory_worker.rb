4f76f673d16d4cee4322ccafe8ece4cb80fa1bdf
jachicao <jachicao@uc.cl>
2017-06-18 12:56:05 -0400

    .

--- app/workers/buy_product_to_factory_worker.rb
+++ app/workers/buy_product_to_factory_worker.rb
@@ -5,6 +5,6 @@ class BuyProductToFactoryWorker < ApplicationWorker
     factory_id = factory_response[:body][:cuentaId]
     transaction_response = self.transfer_money(factory_id, cantidad * costo_unitario)
     transaction_id = transaction_response[:body][:_id]
-    self.make_product(sku, cantidad, transaction_id)
+    return self.make_product(sku, cantidad, transaction_id)
   end
 end
