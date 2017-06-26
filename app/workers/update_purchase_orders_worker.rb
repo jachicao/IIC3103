f8041151695e9d1fcead881febe3d306f22153ed
jachicao <jachicao@uc.cl>
2017-06-26 16:21:41 -0400

    update quantity dispatched correctly

--- app/workers/update_purchase_orders_worker.rb
+++ app/workers/update_purchase_orders_worker.rb
@@ -5,7 +5,7 @@ class UpdatePurchaseOrdersWorker < ApplicationWorker
     PurchaseOrder.all.each do |purchase_order|
       if purchase_order.is_rejected or purchase_order.is_cancelled or purchase_order.is_completed
       else
-        UpdatePurchaseOrderWorker.perform_async(purchase_order.po_id, false)
+        UpdatePurchaseOrderWorker.perform_async(purchase_order.po_id)
       end
     end
   end
