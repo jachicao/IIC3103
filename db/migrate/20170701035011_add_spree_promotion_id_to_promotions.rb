8f4f628d7032b88a4a0b7e7402fdb80a96720482
jachicao <jachicao@uc.cl>
2017-06-30 23:55:17 -0400

    remove spree_promotion_id migration

--- db/migrate/20170701035011_add_spree_promotion_id_to_promotions.rb
+++
@@ -1,5 +0,0 @@
-class AddSpreePromotionIdToPromotions < ActiveRecord::Migration[5.0]
-  def change
-    add_column :promotions, :spree_promotion_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170626174756) do
+ActiveRecord::Schema.define(version: 20170701035212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -137,6 +137,7 @@ ActiveRecord::Schema.define(version: 20170626174756) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "spree_promotion_id"
+    t.integer  "spree_id"
     t.index ["product_id"], name: "index_promotions_on_product_id", using: :btree
   end
 
